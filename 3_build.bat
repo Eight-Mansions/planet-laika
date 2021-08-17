bd213fd93b3f66e0accf1d0d14ab542c45c143da
esperknight <esperknight@yahoo.com>
2021-08-17 01:08:58 -0400

    Fixed a few errors in the merge for the script and all good to go now.

--- 3_build.bat
+++ 3_build.bat
@@ -10,4 +10,9 @@ tools\planet_laika_scene_compress.exe ins cd\laika *.BIN
 
 tools\planet_laika_font_in.exe graphics\font.bmp cd\laika\SYS\LAIKA.FNZ
 
+del cd\laika\STR\M000.STR
+del cd\laika\STR\M064.STR
+copy movies\M000.STR cd\laika\STR\M000.STR
+copy movies\M064.STR cd\laika\STR\M064.STR
+
 tools\psximager\psxbuild.exe -v cd\laika >> cd\build.log
