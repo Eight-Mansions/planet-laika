6ce95374595a9682decb30d2764ff4a900cd4225
esperknight <esperknight@yahoo.com>
2021-10-30 21:08:30 -0400

    Little bit of clean up.  Added letter widths to calculations.  Also fixed newlines. Now to fix "newlines" in VRAM...

--- code/code.asm
+++ code/code.asm
@@ -33,16 +33,29 @@
 .org 0x8001d020
 	j increaseXCoord
 	
+; Increase the allowed width by 2 more pixels to make it so i have a bit more wiggle room
+;.org 0x8001d030
+;	addiu v1, a0, 0x22
+
+;.org 0x8001d028
+;	addiu v0, a3, 0x03
+	
 .org 0x8001d03c
 	addu v0, a1, t1
-
+	
+; -------------------------------
 ; Below is when we hit a newline character (0x0A).  We need to set it to "overflow" so it'll increas the x position (why it has to happen who knows...)
-.org 0x8001d628
-	j newline
+;.org 0x8001d628
+;	j newline
 	
 ; Increase the amount of letters weve seen
 .org 0x8001deac
 	addu v0, v0, t4 ; t4 holds our overflow...
+;------------------------------------
+
+; Setup current width of the letter
+.org 0x8001de9c
+	jal getLetterWidth
 
 ; a3 = text buffer position
 ; t9 = cur width location
@@ -52,8 +65,13 @@
 
 .org 0x80063640
 start_draw_letter:
-	la t3, ovr_buffer
 	la t9, variables
+	la t3, ovr_buffer
+	lb v0, 0(t9) ; cur width
+	addiu v1, r0, 0x0B
+	slt v1, v1, v0
+	sb v1, 2(t9)
+	
 	
 draw_letter:
 	lbu        a0, 0x0(t0) ; Load 1bpp font pixel here
@@ -99,11 +117,10 @@ draw_letter:
 	sll        v0, v0, 0x4
 	or         v1, v1, v0
 	andi       v0, a0, 0xc
-	sllv        v0, v0, v1
+	sllv       v0, v0, v1
 	or         v1, v1, v0
 	andi       a0, a0, 0x3
 	or         v1, v1, a0
-	addu       t4, r0, v1
 	sh		   v1, 8(t9)
 	
 	;sh		   t2, 0(a3)
@@ -121,18 +138,22 @@ draw_letter:
 		
 	do_vwf:
 	
-		lbu t6, 0x02(t9) ; Load if we overflowed, 1 we did, 0 we didn't
+		;lbu t6, 0x02(t9) ; Load if we overflowed, 1 we did, 0 we didn't
 		lbu t2, 0x00(t9) ; Load current width
+		addiu t6, r0, 0x0B
+		slt t6, t6, t2
 		beqz t6, start_vwf 
+		;sb t6, 0x02(t9)
 		nop
 		addiu t2, t2, -0x0C
 		clear_overflow:	
-			lw t4, 0x00(t3) ; overflow
-			lh t5, 0x04(t3)
+			lw a0, 0x00(t3) ; overflow
 			sw r0, 0x00(t3)
+			sw a0, 0x00(a3)
+			
+			lh a0, 0x04(t3)
 			sh r0, 0x04(t3)
-			sw t4, 0x00(a3)
-			sh t5, 0x04(a3)
+			sh a0, 0x04(a3)
 
 	start_vwf:
 		addu t8, r0, t2   ; cur width
@@ -163,7 +184,8 @@ draw_letter:
 				
 				bgtz t8, shift_letter
 				
-				sll v0, v0, 4   ;(char2pixels1 << 4);	
+				sll v0, v0, 4   ;(char2pixels1 << 4);
+				
 			store_letter:
 				; Grab letters written already
 				lw t7, 0x00(a3)
@@ -179,18 +201,6 @@ draw_letter:
 				sw t4, 0x00(t3) ; overflow
 				sh t5, 0x04(t3)
 				
-				
-				;sb r0, 0(t9)
-				
-		; lb t1, 1(t9)
-		; addiu t4, r0, 0x0C ; default next widths to C
-		; sb t4, 1(t9)
-		; addu t2, t2, t1
-		; sltiu t1, t2, 0x0D
-		
-		; sb t2, 0(t9) ; store width
-		; sb t1, 2(t9) ; Store if we overflowed or not (1 means we didn't)	
-	
 	end_vwf:
 		;sh         v1, 0x0(a3) ; Store 4bpp font pixel here
 		slti       v0, a2, 0x30
@@ -198,15 +208,17 @@ draw_letter:
 		bne        v0, r0, draw_letter
 		addiu      a3, a3,0x6   ; Increase pixel buffer store position
 		
-		;lb t4, 1(t9)
+		lb t4, 1(t9)
+		nop
 		;addiu t4, r0, 0x0C ; default next widths to C
 		;sb t4, 1(t9)
-		addiu t2, t2, 0x08
-		addiu t4, r0, 0x0B
-		slt t4, t4, t2
+		;addiu t2, t2, 0x08
+		addu t2, t2, t4
+		;addiu t4, r0, 0x0B
+		;slt t4, t4, t2
 		
 		sb t2, 0(t9) ; store width
-		sb t4, 2(t9) ; Store if we overflowed or not (1 means we didn't)	
+		lb t4, 2(t9) ; load if we overflowed or not (1 means we didn't)	
 		
 		
 		j 0x8001e5e0
@@ -227,11 +239,12 @@ increaseLetterBuffer:
 ;8001d038 : BNE     00000001 (v0), 00000000 (r0), 8001d054,
 ;8001d03c : ADDIU   00000001 (v0), 00000020 (a1), 0003 (3),
 increaseXCoord:
-	la t1, ovr_flow
+	la t1, cur_width
 	lb t1,  0(t1)
 	lb a1, 0x02(s1)
-	beq t1, r0, noIncreaseX
-	nop
+	slti t1, t1, 0x0B
+	bne t1, r0, noIncreaseX
+	xor t1, t1
 	addiu t1, r0, 0x03
 noIncreaseX:
 	j 0x8001d028
@@ -241,16 +254,26 @@ newline:
 	la t1, cur_width
 	lb v0, 0xA1D4(v0)
 	addiu t2, r0, 0x0C
+	;sb r0, 1(t1)
 	sb t2, 0(t1)
 	j 0x8001d630
 	sb t2,  2(t1) ; We don't care what # it is as long as it isn't 0
 	
+getLetterWidth:
+	la t2, letter_widths
+	la t0, nex_width
+	addiu t1, a0, 0xFFE0
+	addu t2, t2, t1
+	lb t2, 0(t2)
+	j 0x8001e518	
+	sb t2, 0(t0)
+	nop
 
 variables:
 cur_width:
 	.db 0
 nex_width:
-	.db 0
+	.db 0x08
 ovr_flow:
 	.db 0
 padding:
@@ -260,5 +283,99 @@ tmp_buffer:
 	.dw 0
 ovr_buffer:
 	.db 0
+
+.org 0x80063900
+letter_widths:
+	.db 0x00
+	.db 0x04
+	.db 0x05
+	.db 0x04
+	.db 0x00
+	.db 0x0A
+	.db 0x09
+	.db 0x03
+	.db 0x06
+	.db 0x06
+	.db 0x0D
+	.db 0x06
+	.db 0x0D
+	.db 0x06
+	.db 0x04
+	.db 0x06
+	.db 0x08
+	.db 0x05
+	.db 0x08
+	.db 0x08
+	.db 0x09
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x04
+	.db 0x00
+	.db 0x00
+	.db 0x06
+	.db 0x00
+	.db 0x08
+	.db 0x00
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x07
+	.db 0x07
+	.db 0x08
+	.db 0x08
+	.db 0x07
+	.db 0x08
+	.db 0x09
+	.db 0x07
+	.db 0x0A
+	.db 0x09
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x07
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x0C
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x00
+	.db 0x00
+	.db 0x00
+	.db 0x00
+	.db 0x00
+	.db 0x00
+	.db 0x08
+	.db 0x08
+	.db 0x07
+	.db 0x08
+	.db 0x08
+	.db 0x07
+	.db 0x08
+	.db 0x08
+	.db 0x04
+	.db 0x07
+	.db 0x08
+	.db 0x04
+	.db 0x0C
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x08
+	.db 0x07
+	.db 0x07
+	.db 0x06
+	.db 0x08
+	.db 0x08
+	.db 0x0C
+	.db 0x08
+	.db 0x08
+	.db 0x08
 
 .close
